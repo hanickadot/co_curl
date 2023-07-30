e6d91b0af3f11044860d86b4a11ed5d6bbbc8038
Hana Dusíková <hanicka@hanicka.net>
2023-07-30 16:43:34 +0200

    returning values between coroutines

--- examples/jobs.cpp
+++ examples/jobs.cpp
@@ -6,7 +6,7 @@ auto test2(co_curl::thread_pool &) -> co_curl::detached_task<int> {
 	std::cout << "test2: wait for " << wait.count() << " seconds.\n";
 	std::this_thread::sleep_for(wait);
 	std::cout << "test2: wait done.\n";
-	co_return 41;
+	co_return 5;
 }
 
 auto test3(co_curl::thread_pool &) -> co_curl::detached_task<int> {
@@ -14,7 +14,7 @@ auto test3(co_curl::thread_pool &) -> co_curl::detached_task<int> {
 	std::cout << "test3: wait for " << wait.count() << " seconds.\n";
 	std::this_thread::sleep_for(wait);
 	std::cout << "test3: wait done.\n";
-	co_return 41;
+	co_return 7;
 }
 
 auto test(co_curl::thread_pool & tp) -> co_curl::detached_task<int> {
@@ -27,25 +27,27 @@ auto test(co_curl::thread_pool & tp) -> co_curl::detached_task<int> {
 	std::this_thread::sleep_for(wait);
 	std::cout << "test1: wait done.\n";
 
-	co_await t2;
+	auto a = co_await t2;
 
 	std::cout << "test1: between co_await\n";
 
-	co_await t3;
+	auto b = co_await t3;
 
 	std::cout << "test1: after co_await\n";
 
-	co_return 42;
+	co_return a * b;
 }
 
 int main() {
 	std::cout << "main()\n";
 	auto tp = co_curl::thread_pool{2};
 
-	test(tp);
+	auto r = co_curl::sync_await(test(tp));
+	std::cout << "when job is done...\n";
 
-	std::this_thread::sleep_for(std::chrono::seconds{3});
 	tp.stop_and_join();
+
+	std::cout << "result = " << r << "\n";
 
 	std::cout << "~main()\n";
 }
--- include/co_curl/thread-pool.hpp
+++ include/co_curl/thread-pool.hpp
@@ -1,24 +1,23 @@
 #ifndef CO_CURL_THREAD_POOL_HPP
 #define CO_CURL_THREAD_POOL_HPP
 
-#include <deque>
+#include <atomic>
 #include <functional>
-#include <iostream>
+#include <future>
+#include <queue>
 #include <thread>
 #include <utility>
+#include <variant>
 #include <vector>
 #include <coroutine>
 
 namespace co_curl {
 
 struct thread_pool {
-	std::deque<std::function<void(void)>> queue{};
+	std::queue<std::coroutine_handle<>> queue{};
 	std::vector<std::thread> threads{};
 	std::mutex mutex{};
 	std::condition_variable cv{};
-
-	struct stop_exception {
-	};
 
 	explicit thread_pool(size_t count) {
 		add_threads(count);
@@ -32,34 +31,28 @@ struct thread_pool {
 	}
 
 	void stop_and_join() {
-		stop_all_threads();
+		for (std::thread & th: threads) {
+			add({});
+		}
+
 		for (std::thread & th: threads) {
 			th.join();
 		}
 		threads.resize(0);
 	}
 
-	auto obtain_task_from_bottom() {
+	auto get_next() -> std::coroutine_handle<> {
 		auto lock = std::unique_lock(mutex);
 		cv.wait(lock, [this] { return !queue.empty(); });
 
 		auto res = std::move(queue.front());
-		queue.pop_front();
+		queue.pop();
 		return res;
 	}
 
-	auto obtain_task_from_top() {
+	void add(std::coroutine_handle<> handle) {
 		auto lock = std::unique_lock(mutex);
-		cv.wait(lock, [this] { return !queue.empty(); });
-
-		auto res = std::move(queue.front());
-		queue.pop_back();
-		return res;
-	}
-
-	template <typename T> void add_task_to_top(T && task) {
-		auto lock = std::unique_lock(mutex);
-		queue.emplace_back(std::forward<T>(task));
+		queue.emplace(handle);
 		cv.notify_one();
 	}
 
@@ -69,71 +62,194 @@ struct thread_pool {
 		}
 	}
 
-	void stop_thread() {
-		add_task_to_top([] { throw stop_exception{}; });
-	}
-
-	void stop_all_threads() {
-		for (auto & th: threads) {
-			stop_thread();
-		}
-	}
-
 	void add_thread() {
 		threads.emplace_back([this] {
-			for (;;) {
-				try {
-					auto task = obtain_task_from_bottom();
-					task();
-				} catch (stop_exception) {
-					break;
-				}
+			while (auto handle = get_next()) {
+				handle.resume();
 			}
 		});
 	}
 };
 
+template <typename R> struct result_or_exception: std::variant<std::monostate, R, std::exception_ptr> {
+	using super = std::variant<std::monostate, R, std::exception_ptr>;
+	using super::super;
+
+	R result() && {
+		if (super::index() == 1) {
+			return std::move(std::get<1>(*this));
+		} else if (super::index() == 2) {
+			std::rethrow_exception(std::get<2>(*this));
+		} else {
+			assert(false);
+			std::terminate();
+		}
+	}
+	R & result() & {
+		if (super::index() == 1) {
+			return std::get<1>(*this);
+		} else if (super::index() == 2) {
+			std::rethrow_exception(std::get<2>(*this));
+		} else {
+			assert(false);
+			std::terminate();
+		}
+	}
+};
+
+template <> struct result_or_exception<void>: std::variant<std::monostate, std::exception_ptr> {
+	using super = std::variant<std::monostate, std::exception_ptr>;
+	using super::super;
+
+	void result() {
+		if (super::index() == 0) {
+			return;
+		} else {
+			std::rethrow_exception(std::get<1>(*this));
+		}
+	}
+};
+
+template <typename Promise> concept with_thread_pool = requires(Promise & promise) {
+	{ promise.pool } -> std::same_as<thread_pool &>;
+};
+
+struct schedule_at_threadpool {
+	bool await_ready() noexcept { return false; }
+	void await_resume() noexcept { }
+	template <with_thread_pool Promise> void await_suspend(std::coroutine_handle<Promise> h) {
+		h.promise().pool.add(h);
+	}
+};
+
+template <typename Promise> concept with_single_awaiter = requires(Promise & promise) {
+	{ promise.awaiter } -> std::convertible_to<std::coroutine_handle<>>;
+};
+
+struct suspend_or_jump_to_awaiter {
+	bool await_ready() noexcept { return false; }
+
+	void await_resume() noexcept { }
+
+	template <with_single_awaiter Promise> auto await_suspend(std::coroutine_handle<Promise> h) noexcept -> std::coroutine_handle<> {
+		if (auto next = h.promise().awaiter) {
+			return next;
+		} else {
+			return std::noop_coroutine(); // let thread_pool select next task
+		}
+	}
+
+	// sink for other promise types
+	void await_suspend(std::coroutine_handle<>) noexcept { }
+};
+
+template <typename R> struct detached_promise_type {
+	thread_pool & pool;
+	std::coroutine_handle<> awaiter{};
+	result_or_exception<R> result{};
+
+	explicit detached_promise_type(thread_pool & t) noexcept: pool{t} { }
+
+	auto initial_suspend() {
+		return schedule_at_threadpool{};
+	}
+
+	auto final_suspend() noexcept {
+		return suspend_or_jump_to_awaiter{};
+	}
+
+	template <std::convertible_to<R> T> void return_value(T && value) {
+		result = std::forward<T>(value);
+	}
+
+	void unhandled_exception() noexcept {
+		result = std::current_exception();
+	}
+
+	auto get_return_object() {
+		return std::coroutine_handle<detached_promise_type>::from_promise(*this);
+	}
+};
+
+template <> struct detached_promise_type<void> {
+	thread_pool & pool;
+	std::coroutine_handle<> awaiter{};
+	result_or_exception<void> result{};
+
+	explicit detached_promise_type(thread_pool & t) noexcept: pool{t} { }
+
+	auto initial_suspend() {
+		return schedule_at_threadpool{};
+	}
+
+	auto final_suspend() noexcept {
+		return suspend_or_jump_to_awaiter{};
+	}
+
+	void return_void() {
+		result = std::monostate{};
+	}
+
+	void unhandled_exception() noexcept {
+		result = std::current_exception();
+	}
+
+	auto get_return_object() {
+		return std::coroutine_handle<detached_promise_type>::from_promise(*this);
+	}
+};
+
 template <typename R> struct detached_task {
+	using promise_type = detached_promise_type<R>;
+	using handle_type = std::coroutine_handle<promise_type>;
+
+	handle_type handle{};
+
+	detached_task(handle_type h) noexcept: handle{h} { }
+	detached_task(const detached_task &) = delete;
+	detached_task(detached_task && other) noexcept: handle{std::exchange(other.handle, nullptr)} { }
+
+	detached_task & operator=(const detached_task &) = delete;
+	detached_task & operator=(detached_task && other) noexcept {
+		std::swap(other.handle, handle);
+		return *this;
+	}
+
+	~detached_task() noexcept {
+		if (handle) {
+			handle.destroy();
+		}
+	}
+
+	bool await_ready() const noexcept {
+		assert(handle);
+		return handle.done();
+	}
+
+	R await_resume() {
+		return handle.promise().result.result();
+	}
+
+	void await_suspend(std::coroutine_handle<> awaiter) {
+		assert(handle.promise().awaiter == nullptr); // we support one awaiter for detached_tasks
+		// attach current task to follow after awaited ends
+		handle.promise().awaiter = awaiter;
+		// and let thread_pool obtain another task
+	}
+};
+
+template <typename T> struct sync_awaiter {
 	struct promise_type {
-		thread_pool & pool;
-		std::coroutine_handle<> awaiter{};
+		std::promise<T> result;
 
-		explicit promise_type(thread_pool & t) noexcept: pool{t} { }
+		auto initial_suspend() { return std::suspend_never{}; }
+		auto final_suspend() noexcept { return std::suspend_always{}; }
 
-		auto initial_suspend() {
-			struct schedule_at_threadpool {
-				bool await_ready() noexcept { return false; }
-				void await_resume() noexcept { }
-				void await_suspend(std::coroutine_handle<promise_type> h) {
-					h.promise().pool.add_task_to_top([=] { h.resume(); });
-				}
-			};
-
-			return schedule_at_threadpool{};
+		void return_value(T value) noexcept {
+			result.set_value(value);
 		}
 
-		auto final_suspend() noexcept {
-			struct suspend_or_jump_to_awaiter {
-				bool await_ready() noexcept { return false; }
-				void await_resume() noexcept { }
-				auto await_suspend(std::coroutine_handle<promise_type> h) noexcept -> std::coroutine_handle<> {
-					if (auto next = h.promise().awaiter) {
-						std::cout << "final suspend -> awaiter\n";
-						return next;
-					} else {
-						std::cout << "final suspend -> select()\n";
-						return std::noop_coroutine(); // let thread_pool select next task
-					}
-				}
-			};
-
-			return suspend_or_jump_to_awaiter{};
-		}
-
-		void return_value(R) {
-		}
-
-		void unhandled_exception() {
+		void unhandled_exception() noexcept {
 			std::terminate();
 		}
 
@@ -143,28 +259,28 @@ template <typename R> struct detached_task {
 	};
 
 	using handle_type = std::coroutine_handle<promise_type>;
-
 	handle_type handle{};
 
-	detached_task(handle_type h) noexcept: handle{h} {
+	sync_awaiter(std::coroutine_handle<promise_type> h) noexcept: handle{h} { }
+	sync_awaiter(const sync_awaiter &) = delete;
+	sync_awaiter(sync_awaiter &&) = delete;
+
+	~sync_awaiter() noexcept {
+		handle.destroy();
 	}
 
-	bool await_ready() const noexcept {
-		assert(handle);
-		return handle.done();
-	}
-
-	R await_resume() {
-		return {};
-	}
-
-	void await_suspend(std::coroutine_handle<> awaiter) {
-		std::cout << "awaiting...\n";
-		// attach current task to follow after awaited ends
-		handle.promise().awaiter = awaiter;
-		// and let thread_pool obtain another task
+	auto get_result() {
+		return handle.promise().result.get_future().get();
 	}
 };
+
+template <typename T> auto sync_await(detached_task<T> && task) {
+	return [&]() -> sync_awaiter<T> { co_return co_await task; }().get_result();
+}
+
+template <typename T> auto sync_await(detached_task<T> & task) {
+	return [&]() -> sync_awaiter<T> { co_return co_await task; }().get_result();
+}
 
 } // namespace co_curl
 
